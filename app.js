65a91034799d35feb364a64e5e6a79b30b5e3858
Abhinav Palugula <abhinav.palugula@gmail.com>
2023-10-15 12:23:47 +0530

    release

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+require("dotenv").config()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 const https = require("https");
-app.listen(process.env.PORT || 2000, () => {});
+app.listen(process.env.PORT || 2000, () => { });
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/input.html");
 });
@@ -13,8 +14,8 @@ app.post("/", (req, res) => {
   var cityName = req.body.cityName;
   https.get(
     "https://api.openweathermap.org/data/2.5/weather?q=" +
-      cityName +
-      "&appid=24717ca57714bfe19073e54a23e8c0de&units=metric",
+    cityName +
+    `&appid=${process.env.appid}&units=metric`,
     (response) => {
       var responseData = "";
       response.on("data", function (data) {
