2c0691af29d0a95796c3aa5f567993c0b3f09571
Abhinav Palugula <abhinav.palugula@gmail.com>
2023-10-15 13:18:31 +0530

    Integrated Error Handling

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 const https = require("https");
 app.listen(process.env.PORT || 2000, () => { });
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/input.html");
+  res.render("input", { city: "", error: false, errorM: "" })
 });
 app.post("/", (req, res) => {
   var cityName = req.body.cityName;
@@ -18,30 +18,33 @@ app.post("/", (req, res) => {
     `&appid=${process.env.appid}&units=metric`,
     (response) => {
       var responseData = "";
-      response.on("data", function (data) {
-        responseData += data;
-      });
-      response.on("end", function () {
-        const jsonData = JSON.parse(responseData);
-        const ResponseCity = jsonData.name;
-        const currentTemp = jsonData.main.temp;
-        const feelsLike = jsonData.main.feels_like;
-        const maxTemp = jsonData.main.temp_max;
-        const minTemp = jsonData.main.temp_min;
-        const description = jsonData.weather[0].description;
-        const icon = jsonData.weather[0].icon;
-        const iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-        console.log(jsonData);
-        res.render("weather", {
-          ResponseCity: ResponseCity,
-          currentTemp: currentTemp,
-          feelsLike: feelsLike,
-          maxTemp: maxTemp,
-          minTemp: minTemp,
-          description: description,
-          iconUrl: iconUrl,
+      if (response.statusCode == 200) {
+        response.on("data", function (data) {
+          responseData += data;
         });
-      });
+        response.on("end", function () {
+          const jsonData = JSON.parse(responseData);
+          const ResponseCity = jsonData.name;
+          const currentTemp = jsonData.main.temp;
+          const feelsLike = jsonData.main.feels_like;
+          const maxTemp = jsonData.main.temp_max;
+          const minTemp = jsonData.main.temp_min;
+          const description = jsonData.weather[0].description;
+          const icon = jsonData.weather[0].icon;
+          const iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+          res.render("weather", {
+            ResponseCity: ResponseCity,
+            currentTemp: currentTemp,
+            feelsLike: feelsLike,
+            maxTemp: maxTemp,
+            minTemp: minTemp,
+            description: description,
+            iconUrl: iconUrl,
+          });
+        });
+      } else if (response.statusCode == 404) {
+        res.render("input", { city: cityName, error: true, errorM: "Invalid City Name" })
+      }
     }
   );
 });
